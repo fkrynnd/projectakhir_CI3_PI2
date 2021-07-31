3784c717ff15123e8824b07391e35d3986965dda
fkrynnd <fikri230600@gmail.com>
2021-07-31 23:55:55 +0700

    project akhir CI3 PI2

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tugas-Pertemuan13-PI2
+Tugas pertemuan 13 Pemrograman Internet 2, menggunakan Codeigniter untuk membuat tabel dengan fungsi (tambah, edit, hapus, detail)
---
+++ pi2_penjualan_barang.sql
@@ -0,0 +1,178 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 31 Jul 2021 pada 18.43
+-- Versi server: 10.4.16-MariaDB
+-- Versi PHP: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pi2_penjualan_barang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `barang_id` int(11) NOT NULL,
+  `nama_barang` varchar(1000) NOT NULL,
+  `harga_barang` int(15) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`barang_id`, `nama_barang`, `harga_barang`, `stok`, `keterangan`) VALUES
+(11, 'ASUS ROG Zephyrus G14 GA401', 14899000, 4, 'Keadaan Laptop Baru'),
+(12, 'HP PAVILION GAMING 15 DK1064TX', 15199000, 2, 'Dapat bonus 1 paket perlengkapan gaming'),
+(13, 'Lenovo Legion Y530-15ICH-72ID Black', 15599000, 3, 'Dapat bonus mouse+keyboard gaming');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `pelanggan_id` int(11) NOT NULL,
+  `nama_pelanggan` varchar(100) NOT NULL,
+  `no_tlp` text NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`pelanggan_id`, `nama_pelanggan`, `no_tlp`, `alamat`) VALUES
+(9, 'Fikri Ananda', '08822569878', 'Perumahan Megu Cilik, Weru, Cirebon'),
+(10, 'Dean Siska', '088745621478', 'Jalan Raya Plered, Cirebon'),
+(11, 'Gustami', '08874589658', 'Jalan Raya Pilang, Cirebon');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `penjualan_id` int(11) NOT NULL,
+  `tgl_penjualan` varchar(50) NOT NULL,
+  `pelanggan_id` int(11) NOT NULL,
+  `keterangan` text NOT NULL,
+  `total` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`penjualan_id`, `tgl_penjualan`, `pelanggan_id`, `keterangan`, `total`) VALUES
+(1627749571, '2021-07-31', 9, 'Membeli 1 unit HP PAVILION GAMING 15 DK1064TX', 43466170),
+(1627749713, '2021-07-31', 10, 'Membeli 2 unit Lenovo Legion Y530-15ICH-72ID Black', 73660150),
+(1627749751, '2021-07-31', 11, 'pelanggan setia toko laptopku', 31198000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan_detail`
+--
+
+CREATE TABLE `penjualan_detail` (
+  `penjualan_detail_id` int(11) NOT NULL,
+  `penjualan_id` int(11) NOT NULL,
+  `barang_id` int(11) NOT NULL,
+  `harga_total` int(20) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `diskon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penjualan_detail`
+--
+
+INSERT INTO `penjualan_detail` (`penjualan_detail_id`, `penjualan_id`, `barang_id`, `harga_total`, `jumlah`, `diskon`) VALUES
+(13, 1627749571, 11, 14452030, 1, 3),
+(14, 1627749571, 11, 29798000, 2, 0),
+(15, 1627749571, 13, 29014140, 2, 7),
+(16, 1627749713, 13, 31198000, 2, 0),
+(17, 1627749713, 11, 42462150, 3, 5),
+(18, 1627749751, 13, 31198000, 2, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`barang_id`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`pelanggan_id`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`penjualan_id`);
+
+--
+-- Indeks untuk tabel `penjualan_detail`
+--
+ALTER TABLE `penjualan_detail`
+  ADD PRIMARY KEY (`penjualan_detail_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `barang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `pelanggan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `penjualan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1627749752;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan_detail`
+--
+ALTER TABLE `penjualan_detail`
+  MODIFY `penjualan_detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
